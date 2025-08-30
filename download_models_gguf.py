eef8c589d58d9fa17d7939647e10d8895380af24
maxolgi <maxolgi@proton.me>
2025-08-30 17:34:36 -0400

    Update download_models_gguf.py

--- download_models_gguf.py
+++ download_models_gguf.py
@@ -13,7 +13,7 @@ api = HfApi()
 files = api.list_repo_files(args.repo_id)
 
 safetensors_files = [f for f in files if f.endswith('.safetensors')]
-gguf_files = [f for f in gguf_files if f.endswith('.gguf')]
+gguf_files = [f for f in files if f.endswith('.gguf')]
 
 if args.q:
     if '_' in args.q:
@@ -23,7 +23,22 @@ if args.q:
 
 gguf_dir = os.path.join(args.local_dir, "unet")
 vae_dir = os.path.join(args.local_dir, "vae")
-to_download = [(f, vae_dir) for f in safetensors_files] + [(f, gguf_dir) for f in gguf_files]
+text_encoder_dir = os.path.join(args.local_dir, "text_encoder")
+
+to_download = []
+for f in safetensors_files:
+    if "encoder" in f.lower():
+        dir_path = text_encoder_dir
+    else:
+        dir_path = vae_dir
+    to_download.append((f, dir_path))
+
+for f in gguf_files:
+    if "encoder" in f.lower():
+        dir_path = text_encoder_dir
+    else:
+        dir_path = gguf_dir
+    to_download.append((f, dir_path))
 
 for f, dir_path in to_download:
     base_name = os.path.basename(f)
