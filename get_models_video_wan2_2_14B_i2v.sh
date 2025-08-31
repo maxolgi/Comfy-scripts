41da116776eb87d65e00014ddda502736880b614
maxolgi <maxolgi@proton.me>
2025-08-31 12:47:31 -0400

    Update get_models_video_wan2_2_14B_i2v.sh

--- get_models_video_wan2_2_14B_i2v.sh
+++ get_models_video_wan2_2_14B_i2v.sh
@@ -2,3 +2,7 @@
 python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-I2V-A14B-GGUF &
 python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M city96/umt5-xxl-encoder-gguf &
 python /ComfyUI/Comfy-scripts/download_models.py /ComfyUI/Comfy-scripts/video_wan2_2_14B_i2v-gguf.json &
+
+git clone https://github.com/city96/ComfyUI-GGUF /ComfyUI/custom_nodes/ComfyUI-GGUF
+git checkout d247022e3fa66851c5084cc251b076aab816423d
+pip install --upgrade gguf
