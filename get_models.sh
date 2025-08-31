02d826cf31585057cbed6f6498cdd6639a6d18b4
maxolgi <maxolgi@proton.me>
2025-08-31 00:14:14 -0400

    Update get_models.sh

--- get_models.sh
+++ get_models.sh
@@ -4,4 +4,4 @@ python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/mode
 python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-T2V-A14B-GGUF &
 python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-I2V-A14B-GGUF &
 python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M city96/umt5-xxl-encoder-gguf &
-python3 /ComfyUI/Comfy-scripts/download_models.py --watch --daemon --directory /ComfyUI/user/default/workflows/
+python3 /ComfyUI/Comfy-scripts/download_models.py wan2.2_t2v_lightx2v_4steps_lora_v1.1_low_noise.safetensors
