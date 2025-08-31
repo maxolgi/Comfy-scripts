cb0836821e96536d78dc1ffce2e83d639d5cd7ea
maxolgi <maxolgi@proton.me>
2025-08-31 14:46:25 -0400

    Update get_models.sh

--- get_models.sh
+++ get_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-S2V-14B-GGUF &
-python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-TI2V-5B-GGUF &
-python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-T2V-A14B-GGUF &
-python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-I2V-A14B-GGUF &
-python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /ComfyUI/models --q Q3_K_M city96/umt5-xxl-encoder-gguf &
-python3 /ComfyUI/Comfy-scripts/download_models.py /ComfyUI/Comfy-scripts/gen-img-vid-52.json
+python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /workspace/ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-S2V-14B-GGUF &
+python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /workspace/ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-TI2V-5B-GGUF &
+python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /workspace/ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-T2V-A14B-GGUF &
+python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /workspace/ComfyUI/models --q Q3_K_M QuantStack/Wan2.2-I2V-A14B-GGUF &
+python3 /ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /workspace/ComfyUI/models --q Q3_K_M city96/umt5-xxl-encoder-gguf &
+python3 /ComfyUI/Comfy-scripts/download_models.py /workspace/ComfyUI/Comfy-scripts/gen-img-vid-52.json
