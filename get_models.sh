b9674edf9202cbdeb8b6aa4aa95f953a6e33f618
maxolgi <maxolgi@proton.me>
2025-09-06 09:12:59 -0400

    Update get_models.sh

--- get_models.sh
+++ get_models.sh
@@ -10,4 +10,6 @@ wget --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/C
 wget --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors &
 wget --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors &
 wget --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors &
+wget --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_fun_control_low_noise_14B_fp8_scaled.safetensors &
+wget --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_fun_control_high_noise_14B_fp8_scaled.safetensors &
 wget --directory-prefix=/workspace/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
