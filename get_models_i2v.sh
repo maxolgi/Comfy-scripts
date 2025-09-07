fe21356f108be35c88c308f725c327ece2f09a62
maxolgi <maxolgi@proton.me>
2025-09-07 12:04:13 -0400

    Update get_models_i2v.sh

--- get_models_i2v.sh
+++ get_models_i2v.sh
@@ -3,9 +3,9 @@
 #python3 /workspace/ComfyUI/Comfy-scripts/download_models_gguf.py --local_dir /workspace/ComfyUI/models --q Q3_K_M city96/umt5-xxl-encoder-gguf &
 #wget --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors &
 #wget --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors &
-wget --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors &
-wget --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors &
-wget --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors &
-wget --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors &
-wget --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors &
-wget --directory-prefix=/workspace/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
+wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
