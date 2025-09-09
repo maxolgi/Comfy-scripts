33335e91a3035cd2fadf0763f241f18f964dc7fc
maxolgi <maxolgi@proton.me>
2025-09-08 21:18:53 -0400

    Update get_models_t2s.sh

--- get_models_t2s.sh
+++ get_models_t2s.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -q --directory-prefix=/workspace/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/stable-audio-open-1.0_repackaged/resolve/main/stable-audio-open-1.0.safetensors &
-wget -q --directory-prefix=/workspace/ComfyUI/models/text-encoders https://huggingface.co/ComfyUI-Wiki/t5-base/resolve/main/t5-base.safetensors &
-wget -q --directory-prefix=/workspace/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/ACE-Step_ComfyUI_repackaged/resolve/main/all_in_one/ace_step_v1_3.5b.safetensors &
+wget -q --directory-prefix=/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/stable-audio-open-1.0_repackaged/resolve/main/stable-audio-open-1.0.safetensors &
+wget -q --directory-prefix=/ComfyUI/models/text-encoders https://huggingface.co/ComfyUI-Wiki/t5-base/resolve/main/t5-base.safetensors &
+wget -q --directory-prefix=/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/ACE-Step_ComfyUI_repackaged/resolve/main/all_in_one/ace_step_v1_3.5b.safetensors &
