2e7a8e5f0af94c2014fa3f391e83b4c64cebb75b
maxolgi <maxolgi@proton.me>
2025-09-12 22:14:27 -0400

    Create off.sh

---
+++ off.sh
@@ -0,0 +1,16 @@
+prev=$(find /workspace/ComfyUI/output -type f -exec stat -c '%W %n' {} + | sort -k1,1nr | head -n 1 | cut -d' ' -f2-)
+counter=0
+while true; do
+  sleep 60
+  current=$(find /workspace/ComfyUI/output -type f -exec stat -c '%W %n' {} + | sort -k1,1nr | head -n 1 | cut -d' ' -f2-)
+  if [ "$current" = "$prev" ]; then
+    counter=$((counter + 1))
+  else
+    prev="$current"
+    counter=0
+  fi
+  if [ $counter -ge 2 ]; then
+    runpodctl remove pod $RUNPOD_POD_ID
+    exit 0
+  fi
+done
