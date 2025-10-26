912b7fc242532a40a42f2423df830151984ad95f
maxolgi <maxolgi@proton.me>
2025-10-25 21:26:07 -0400

    Update get_models_i2v.sh

--- get_models_i2v.sh
+++ get_models_i2v.sh
@@ -2,8 +2,8 @@
 wget -q --directory-prefix=/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/stable-audio-open-1.0_repackaged/resolve/main/stable-audio-open-1.0.safetensors &
 wget -q --directory-prefix=/ComfyUI/models/text_encoders https://huggingface.co/ComfyUI-Wiki/t5-base/resolve/main/t5-base.safetensors &
 wget -q --directory-prefix=/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/ACE-Step_ComfyUI_repackaged/resolve/main/all_in_one/ace_step_v1_3.5b.safetensors &
-#wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors &
-#wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_low_noise.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_i2v_lightx2v_4steps_lora_v1_high_noise.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/vae https://huggingface.co/calcuis/wan-gguf/resolve/main/wan_2.1_vae_fp8_e4m3fn.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors &
