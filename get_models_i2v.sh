604212d4bc67608ac70652e9a92963d7dbac2532
maxolgi <maxolgi@proton.me>
2025-10-25 20:54:00 -0400

    Update get_models_i2v.sh

--- get_models_i2v.sh
+++ get_models_i2v.sh
@@ -15,3 +15,4 @@ wget -q --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggi
 wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/lightx2v/Qwen-Image-Lightning/resolve/main/Qwen-Image-Lightning-4steps-V1.0.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Qwen-Image-Edit_ComfyUI/resolve/main/split_files/diffusion_models/qwen_image_edit_2509_fp8_e4m3fn.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors &
+wget -q --directory-prefix=/workspace/ComfyUI/input https://github.com/maxolgi/Comfy-scripts/raw/refs/heads/main/rejected.mp4
