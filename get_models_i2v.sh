19548a3d262b65d066eadac0aa8e5bbbc897a6cd
maxolgi <maxolgi@proton.me>
2025-11-15 17:53:16 -0500

    Update get_models_i2v.sh

--- get_models_i2v.sh
+++ get_models_i2v.sh
@@ -9,8 +9,8 @@ wget -q --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggi
 wget -q --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_high_noise_14B_fp8_scaled.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_i2v_low_noise_14B_fp8_scaled.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/vae https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
-wget -q --directory-prefix=/workspace/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/ACE-Step_ComfyUI_repackaged/resolve/main/all_in_one/ace_step_v1_3.5b.safetensors &
-wget -q --directory-prefix=/workspace/ComfyUI/models/checkpoints https://huggingface.co/Comfy-Org/stable-audio-open-1.0_repackaged/resolve/main/stable-audio-open-1.0.safetensors &
+
+
 wget -q --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggingface.co/ComfyUI-Wiki/t5-base/resolve/main/t5-base.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/text_encoders https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors &
 wget -q --directory-prefix=/workspace/ComfyUI/models/loras https://huggingface.co/lightx2v/Qwen-Image-Lightning/resolve/main/Qwen-Image-Lightning-4steps-V1.0.safetensors &
